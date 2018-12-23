55339e618122313b2345ba534d19ea380d4b62e9
Christopher Hein <chrishein@ChristophersMBP.attlocal.net>
2018-12-23 12:34:05 -0500

    user buttons appear on screen

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,5 +1,6 @@
 // array to get buttons started
-let array = ["Mario", "Luigi", "Link", "Kirby", "Yoshi", "Falco"]
+let array = ["Mario", "Luigi", "Link", "Kirby", "Yoshi", "Falco"];
+let userArray = [];
 
 // create buttons for each item in the array
 for (let i = 0; i < array.length; i++) {
@@ -14,7 +15,12 @@ $(".characterButton").on("click", function(){
     console.log($(this).val());
 })
 
+// create new button based on user input
 $(".submit").on("click", function(){
     event.preventDefault();
+    let newButton = $("<button>");
+    newButton.text($("#input").val()).addClass("newButton").attr("value", $("#input").val());
+    $(".newButtonBar").append(newButton);
+
     $("#input").val("");
 })
