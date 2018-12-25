02e3875fb07b30ba3fe2863dfce09bf6862e8422
Christopher Hein <chrishein@Christophers-MacBook-Pro.local>
2018-12-25 11:19:30 -0500

    Gif Rating appears on page before gifs

--- assets/javascript/api.js
+++ assets/javascript/api.js
@@ -1,15 +1,23 @@
-let character = "Mario";
-let searchLimit = 10;
-let queryURL = "https://api.giphy.com/v1/gifs/search?api_key=FKFrLVSFTOXUw5CIZJKmbKJLXAALG0Af&q=" + character + "&limit=" + searchLimit + "&offset=0&rating=PG&lang=en"
+function getGifs(characterClick) {
 
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function (response) {
-    console.log(response);
-    for (let g = 0; g < searchLimit; g++) {
-        let image = $("<img>")
-        image.attr("src", response.data[g].images.original.url)
-        $(".gifHolder").append(image);
-    }
-});
+let character = characterClick;
+let searchLimit = 10;
+let queryURL = "https://api.giphy.com/v1/gifs/search?api_key=FKFrLVSFTOXUw5CIZJKmbKJLXAALG0Af&q=" + character + "&limit=" + searchLimit + "&offset=0&rating=PG&lang=en&rating=g?&rating=pg"
+
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+        for (let g = 0; g < searchLimit; g++) {
+            let image = $("<img>");
+            let gifRating = $("<p>");
+            gifRating.text("Gif Rating: " + response.data[g].rating);
+            console.log(response.data[g].rating);
+            image.attr("src", response.data[g].images.original.url)
+            $(".gifHolder").prepend(image);
+            $(".gifHolder").prepend(gifRating);
+        }
+    })
+};
