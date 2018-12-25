6395c25f033a6cc3671f8ef42ecad076108bda03
Christopher Hein <chrishein@Christophers-MacBook-Pro.local>
2018-12-25 11:15:50 -0500

    generated button click events work

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,26 +1,39 @@
 // array to get buttons started
-let array = ["Mario", "Luigi", "Link", "Kirby", "Yoshi", "Falco"];
+let array = ["Mario", "Luigi", "Link", "Kirby", "Yoshi", "Captain Falcon"];
 let userArray = [];
 
 // create buttons for each item in the array
 for (let i = 0; i < array.length; i++) {
     let button = $("<button>");
     button.text(array[i]).attr("id", array[i]).attr("value", array[i]).addClass("characterButton");
-
     $(".buttonBar").append(button)
 }
 
 // register value when button is clicked
-$(".characterButton").on("click", function(){
+/*$(".characterButton").on("click", function () {
     console.log($(this).val());
-})
+    // return $(this).val();
+    getGifs($(this).val());
+})*/
 
 // create new button based on user input
-$(".submit").on("click", function(){
+$(".submit").on("click", function () {
     event.preventDefault();
     let newButton = $("<button>");
-    newButton.text($("#input").val()).addClass("newButton").attr("value", $("#input").val());
+    newButton.text($("#input").val()).addClass("newButton characterButton").attr("value", $("#input").val());
     $(".newButtonBar").append(newButton);
+
+    // check array for existing value
+    //if (("#input").val() === )
+
+    // push new items to userarray
+    userArray.push($("#input").val());
+    console.log(userArray);
 
     $("#input").val("");
 })
+
+// register value when button is clicked
+$(document).on("click", ".characterButton", function () {
+    getGifs($(this).val());
+});
