c9f929b11411950aaa8142fbd3ca826db9ebf489
Christopher Hein <chrishein@ChristophersMBP.attlocal.net>
2018-12-27 09:17:11 -0500

    prevent existing words/buttons from being duplicated

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -13,15 +13,17 @@ for (let i = 0; i < array.length; i++) {
 $(".submit").on("click", function () {
     event.preventDefault();
     let newButton = $("<button>");
-    newButton.text($("#input").val().trim()).addClass("newButton characterButton").attr("value", $("#input").val().trim());
-    $(".newButtonBar").append(newButton);
 
     // check array for existing value
-    //if (("#input").val() === )
-
-    // push new items to userarray
-    userArray.push($("#input").val().trim());
+    if (userArray.includes($("#input").val().toLowerCase())) {
+        console.log("already here, bruh!")
+    } else {
+        userArray.push($("#input").val().trim().toLowerCase());
+        newButton.text($("#input").val().trim()).addClass("newButton characterButton").attr("value", $("#input").val().trim());
+        $(".newButtonBar").append(newButton);
+    }
     $("#input").val("");
+    console.log(userArray);
 })
 
 // register value when button is clicked
@@ -29,16 +31,13 @@ $(document).on("click", ".characterButton", function () {
     getGifs($(this).val());
 });
 
+// change src from still to animated src on click event
 $(document).on("click", "img", function () {
     let state = $(this)[0].classList[0]
     if (state === "still") {
-        $(this).removeClass("still");
-        $(this).addClass("animated");
-        $(this).attr("src", $(this).attr("animated"))
+        $(this).removeClass("still").addClass("animated").attr("src", $(this).attr("animated"));
     } else {
-        $(this).removeClass("animated");
-        $(this).addClass("still");
-        $(this).attr("src", $(this).attr("still"))
+        $(this).removeClass("animated").addClass("still").attr("src", $(this).attr("still"))
     }
-    
+
 });
