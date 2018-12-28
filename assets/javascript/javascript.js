4fc65fcbfe1244a176164d758748609dc6d04808
Christopher Hein <chrishein@ChristophersMBP.attlocal.net>
2018-12-27 22:35:47 -0500

    more refactoring of code

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -3,6 +3,12 @@ let array = ["Mario", "Luigi", "Link", "Kirby", "Yoshi", "Captain Falcon"];
 let userArray = [];
 let storedUserInput = [];
 
+function createButton(){
+    let newButton = $("<button>");
+    newButton.text($("#input").val().trim()).addClass("newButton characterButton").attr("value", $("#input").val().trim());
+    $(".newButtonBar").append(newButton);
+}
+
 // create buttons for each item in the array
 for (let i = 0; i < array.length; i++) {
     let button = $("<button>");
@@ -13,16 +19,14 @@ for (let i = 0; i < array.length; i++) {
 // create new button based on user input
 $(".submit").on("click", function () {
     event.preventDefault();
-    let newButton = $("<button>");
     let userInput = $("#input").val().trim().toLowerCase()
 
     // check array for existing value
     if (!userArray.includes(userInput)) {
         userArray.push(userInput);
-        newButton.text($("#input").val().trim()).addClass("newButton characterButton").attr("value", $("#input").val().trim());
-        $(".newButtonBar").append(newButton);
         storedUserInput.push(JSON.stringify(userInput))
         localStorage.setItem("User Input", storedUserInput);
+        createButton();
     }
     $("#input").val("");
     console.log(userArray);
